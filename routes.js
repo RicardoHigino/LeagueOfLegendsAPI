24f12bfb2e17be51214eb06b3824bbb1eb3a4999
Ricardo <ricard.code@gmail.com>
2021-09-08 22:18:58 -0300

    Route name

--- routes.js
+++ routes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const route = express.Router();
 const lolController = require('./src/controllers/lolController')
 
+// Summoner
 route.get('/summoner/:summonerName', lolController.summoner)
+// Champion-Mastery
 route.get('/summoner/champion-mastery/:summonerID', lolController.summonerChampionMastery)
+// Summoner Ranked
 route.get('/summoner/ranked/:rankedName/:summonerName', lolController.summonerRank)
+// Summoner Spectator
 route.get('/summoner/spectator/:summonerName', lolController.summonerSpectator)
 
 module.exports = route;
